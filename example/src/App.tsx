37c61673e8a9bff096463b7bdbbb99c857b9c4df
Nitesh Balusu <niteshbalusu@icloud.com>
2024-10-08 17:08:27 -0400

    fix name

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply, connectToTorNetwork } from 'react-native-tor';
+import { multiply, connectToTorNetwork } from 'react-native-lnd-tor';
 
 export default function App() {
   const [result, setResult] = useState<number | undefined>();
